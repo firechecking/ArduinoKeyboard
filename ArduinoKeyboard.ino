e7010ad7e97bd7f6b4d722dc0649638030f51a83
Patrick <patrick.zzz@alibaba-inc.com>
2021-12-06 15:30:46 +0800

    add  comments

--- ArduinoKeyboard.ino
+++ ArduinoKeyboard.ino
@@ -7,15 +7,15 @@ const int keyTime2 = 500;
 const int keyTime3 = 80;
 const int pinValueBuffer = 5;
 const int inPins[] = {A0, A1, A2, A3};
-const int outPins[] = {3, 4, 5, 6, 7, 8, 9, 10, 16, 14, 15};
+const int outPins[] = {3, 4, 5, 6, 7, 8, 9, 10, 16, 14, 15}; // （输出，输入）构成二维矩阵，输出pin依次输出LOW，然后在输入pin依次判断电压是否被拉低，
 //const int inPins[] = {PD2, PD3, PD4, PD5, PD6, PD7, PD8, PD9, PD10, PD11, PD12};
 
-int pinValues[NUM_OUTPUT][NUM_INPUT];
+int pinValues[NUM_OUTPUT][NUM_INPUT]; // 按键持续计数，按下+1，没按下-1，与pinValueBuffer结合，用于去除噪音抖动
 int pinState[NUM_OUTPUT][NUM_INPUT];
 int pinUpdateMs[NUM_OUTPUT][NUM_INPUT];
 int sendCount[NUM_OUTPUT][NUM_INPUT];
 String preKeys = "";
-String defaultKeys="";
+String defaultKeys = "";
 
 void setup()
 {
@@ -62,20 +62,21 @@ void updateKeyState()
         {
             if (pinValues[i][j] >= 2)
             {
-                keys += '1';
+                keys += '1'; // 按下
             }
             else if (pinValues[i][j] == 0)
             {
-                keys += '0';
+                keys += '0'; // 释放
             }
             else
-                keys += preKeys[i * NUM_INPUT + j];
+                keys += preKeys[i * NUM_INPUT + j]; // 计数不足，保持上一次状态（防止抖动造成按键状态改变）
         }
     }
-    if (keys != preKeys){
-      preKeys = keys;
-//      Serial.println(keys);
-      ConvertKeyToHex(keys);
+    if (keys != preKeys) //只有按键状态发生改变，才需要重新发送
+    {
+        preKeys = keys;
+        //      Serial.println(keys);
+        ConvertKeyToHex(keys); // 转换成串口消息，并发送给上位机 TODO：改为usb keyboard
     }
 }
 
--- KeyMaps.ino
+++ KeyMaps.ino
@@ -1,26 +1,31 @@
 
-void ConvertKeyToHex(String keyStr) {
-  int i, j;
-  int keyValue[11] = {};
-  for (i = 4; i <= keyStr.length(); i += 4) {
-    int subValue = 0;
-    int power = 1;
-    String subKey = keyStr.substring(i - 4, i);
-    //    Serial.println("subKey_"+String(i)+"="+subKey);
-    for (j = 0; j < subKey.length(); j++) {
-      subValue += subKey.substring(j, j + 1).toInt() * power;
-      //      Serial.println("  subValue_"+String(j)+"="+String(subValue));
-      power = power * 4;
+void ConvertKeyToHex(String keyStr)
+{
+    int i, j;
+    int keyValue[11] = {};
+    for (i = 4; i <= keyStr.length(); i += 4)
+    {
+        int subValue = 0;
+        int power = 1;
+        String subKey = keyStr.substring(i - 4, i); // 每个按键有0/1两种状态，并考虑到后续扩展，为节省传输，每个按键状态用2位存储，1个字节保存4个按键状态
+        //    Serial.println("subKey_"+String(i)+"="+subKey);
+        for (j = 0; j < subKey.length(); j++)
+        {
+            subValue += subKey.substring(j, j + 1).toInt() * power;
+            //      Serial.println("  subValue_"+String(j)+"="+String(subValue));
+            power = power * 4;
+        }
+        if (subValue > 0)
+        {
+            keyValue[i / 4 - 1] = subValue;
+        }
     }
-    if (subValue > 0) {
-      keyValue[i / 4 - 1] = subValue;
+    //  Serial.print(":");
+    for (i = 0; i < 11; i++) // 每次发送11个字节
+    {
+        Serial.write(keyValue[i]);
+        //    Serial.print(keyValue[i]);
+        //    Serial.print("-");
     }
-  }
-  //  Serial.print(":");
-  for (i = 0; i < 11; i++) {
-      Serial.write(keyValue[i]);
-//    Serial.print(keyValue[i]);
-//    Serial.print("-");
-  }
-//  Serial.println();
+    //  Serial.println();
 }
